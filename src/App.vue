6951d58bfecef6311a62cbecdf487889ec75fc04
Lucivando Sousa <lucivandosousa@gmail.com>
2024-10-28 16:48:36 -0300

    feat: update App

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-  <h1>Gerador de frases motivacionais</h1>
+  <div class="bg-orange-300 m-auto h-screen p-10 flex flex-col items-center justify-center space-y-4">
+    <h1 class="text-center text-2xl font-bold mb-4 text-gray-800">Receba sua motivação para malhar todos os dias.</h1>
+    <PhraseGenerator />
+  </div>
 </template>
 
 <script setup lang="ts">
+import PhraseGenerator from "./components/phraseGenerator.vue";
 </script>
